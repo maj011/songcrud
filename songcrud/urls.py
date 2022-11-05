d356cfd73e43df298e4005b6ba8d1787eeab7270
Abdul-Jamiu <114449538+maj011@users.noreply.github.com>
2022-11-05 09:23:19 +0000

    Update urls.py

--- songcrud/urls.py
+++ songcrud/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from musicapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+#    path('musicapp/', views.ArtistListApiView.as_view()),
+    path('musicapp/', views.SongListApiView.as_view()),
+#    path('musicapp/<int:pk>', views.song_detail),
 ]
